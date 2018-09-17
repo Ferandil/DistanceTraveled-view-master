ff49d5d64df3c01024ec6d70803a689be3031ce7
Alex <xemyly@yandex.ru>
2018-09-17 20:05:13 +0300

    updated routecontroller

--- src/main/java/userservice/controllers/RouteController.java
+++ src/main/java/userservice/controllers/RouteController.java
@@ -3,12 +3,13 @@ package userservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
-import userservice.controllers.requestbody.RouteBody;
+import org.springframework.web.context.request.WebRequest;
 import userservice.model.Route;
 import userservice.service.RouteService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,8 +23,14 @@ public class RouteController {
     }
 
     @GetMapping(value = "/routes")
-    public @ResponseBody Iterable<Route> getRoutes(@RequestBody RouteBody routeBody){
-        Iterable<Route> list = routeService.findRutesForCurentDates(routeBody.getDateBegin(), routeBody.getDateEnd());
-        return list;
+    public @ResponseBody List<Route> getRoutes(WebRequest request){
+        List<Route> routesList = new ArrayList<>();
+        List<Long> dateList = new ArrayList<>();
+
+        request.getParameterMap().forEach((name, date) -> {
+            dateList.add(Long.valueOf(date[0]));
+        });
+        routeService.findRutesForCurentDates(dateList.get(0), dateList.get(1)).forEach(routesList::add);
+        return routesList;
     }
 }
